98aeb254657e26c84af520c198bd4b719b014bcd
ARUN VIJAY <ARUN VIJAY@ARUNVJX1>
2022-11-10 00:02:33 +0000

    Add project files.

---
+++ newmethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using newmethod.com.Mainclass;
+
+namespace newmethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("New Keyword Used To Inherit the same Name Function in the Main Class");
+            
+            Console.ReadLine();
+            
+            Product mainclass = new Product();
+            mainclass.inherit();
+
+            Console.ReadLine() ;
+            
+        
+        
+        }
+    }
+}
---
+++ newmethod/com/Mainclass/main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newmethod.com.Mainclass
+{
+    class Product :toInherit
+    {
+        
+        new public void inherit()
+        {
+            Console.WriteLine("Inherit with New Keyword");
+            Console.ReadLine();
+        }
+    }
+
+}
